2a7b40040f37f1d16e88d7a6f28d154f316e097f
Roman Paradeev <grumpy@yandex-team.ru>
2017-03-25 22:25:17 +0500

    cleanup

--- django_geocoder/geocoder/management/commands/process_requests.py
+++ django_geocoder/geocoder/management/commands/process_requests.py
@@ -3,24 +3,23 @@ from geocoder.models import Geo
 from geocoder.services.api import Api, ApiError
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
-
-    def _ok(self, message):
-        self.stdout.write(self.style.SUCCESS(message))
+    help = 'Process pending geocode requests'
 
     def handle(self, *args, **options):
         geo_items = Geo.objects.filter(status=Geo.NEW)
         for geo in geo_items:
-            self._ok(geo.address)
             api = Api.withProvider(geo.provider)
             try:
                 response = api.geocode(geo.address.text)
             except ApiError as e:
-                print('{}: {}'.format(type(e).__name__, e))
+                self.stderr.write('{}: {}'.format(type(e).__name__, e))
                 geo.status = geo.ERROR
             else:
                 geo.latitude = response['latitude']
                 geo.longitude = response['longitude']
                 geo.status = geo.SUCCESS
             finally:
+                self.stdout.write(
+                    'Successfully processed: "{}" for {}'
+                        .format(geo.address.text, geo.provider))
                 geo.save()
